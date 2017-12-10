855c77a40e3409e9574626c7e5fa5349ab4d0170
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-12-10 11:59:58 +0900

    Fix order of assert_equal and simplification

--- test/convolution_reverb_test.rb
+++ test/convolution_reverb_test.rb
@@ -3,38 +3,26 @@ require_relative '../src/convolution_reverb'
 
 class TestConvolutionReverb < Test::Unit::TestCase
   def test_ceil_cond1
-    actual = ceil(1.0001)
-    expected = 2
-
-    assert_equal expected, actual
+    assert_equal 2, ceil(1.0001)
   end
 
   def test_ceil_cond2
-    actual = ceil(2)
-    expected = 3
-
-    assert_equal expected, actual
+    assert_equal 3, ceil(2)
   end
 
   def test_nextpow2_cond1
-    actual = nextpow2(65_536)
-    expected = 17
-
-    assert_equal actual, expected
+    assert_equal 17, nextpow2(65_536)
   end
 
   def test_nextpow2_cond2
-    actual = nextpow2(0.5)
-    expected = 0
-
-    assert_equal actual, expected
+    assert_equal 0, nextpow2(0.5)
   end
 
   def test_zeros_cond1
-    assert_equal zeros(3), [0, 0, 0]
+    assert_equal [0, 0, 0], zeros(3)
   end
 
   def test_zeros_cond2
-    assert_equal zeros(0), []
+    assert_equal [], zeros(0)
   end
 end
