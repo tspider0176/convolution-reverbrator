25399f449c8345b5f8e17adb596cfedec99d72dd
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-12-10 11:49:58 +0900

    Add tests for porting functions

--- test/convolution_reverb_test.rb
+++ test/convolution_reverb_test.rb
@@ -2,10 +2,39 @@ require 'test/unit'
 require_relative '../src/convolution_reverb'
 
 class TestConvolutionReverb < Test::Unit::TestCase
-  def test_ceil
+  def test_ceil_cond1
     actual = ceil(1.0001)
     expected = 2
 
     assert_equal expected, actual
+  end
+
+  def test_ceil_cond2
+    actual = ceil(2)
+    expected = 3
+
+    assert_equal expected, actual
+  end
+
+  def test_nextpow2_cond1
+    actual = nextpow2(65_536)
+    expected = 17
+
+    assert_equal actual, expected
+  end
+
+  def test_nextpow2_cond2
+    actual = nextpow2(0.5)
+    expected = 0
+
+    assert_equal actual, expected
+  end
+
+  def test_zeros_cond1
+    assert_equal zeros(3), [0, 0, 0]
+  end
+
+  def test_zeros_cond2
+    assert_equal zeros(0), []
   end
 end
